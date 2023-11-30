cf24fd16ec96a35138f00026c79d76248fa5b049
Shristi Gupta <gshristi40@gmail.com>
2023-11-30 13:21:02 +0530

    changed border colour & position of a card

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"blue-50":"#F4F7FD",
+				"blue-350": "#667EEA",
+			  },
+		},
 	},
 	plugins: [],
 }
