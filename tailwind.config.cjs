354cfc1a0bbe9058cedb19c31658da10636edf5b
Shristi Gupta <gshristi40@gmail.com>
2024-02-11 13:47:55 +0530

    added tags components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"blue-50":"#F4F7FD",
-				"blue-350": "#667EEA",
-			  },
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-50": "#F4F7FD",
+        "blue-350": "#667EEA",
+        "green-200": "#81C042",
+        "purple-400": "#5e0087",
+        "orange-200": "#E3763A",
+      },
+    },
+  },
+  plugins: [],
+};
