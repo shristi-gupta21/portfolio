e6ae4d0672a167dd35ace0385cab8bccaf384845
shristi gupta <gshristi40@gmail.com>
2024-11-24 15:18:28 +0530

    build error resolved

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,5 @@
-import rehypePrism from '@mapbox/rehype-prism'
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    outputFileTracingIncludes: {
-      '/work/*': ['./src/app/work/**/*.mdx'],
-    },
-  },
+  distDir: 'dist',
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
